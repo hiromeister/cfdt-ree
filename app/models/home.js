b72647c45f68b7c0e2ddd4da3f4bc155c04d3960
brun974 <beguebruno@orange.fr>
2017-10-26 13:29:06 +0400

    all ok

---
+++ app/models/home.js
@@ -0,0 +1,34 @@
+
+//app/models/user.js
+//load the things we need
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+//define the schema for our user model
+var userSchema = mongoose.Schema({	
+	
+	nom: String,
+	prenom: String,
+	Syndicat: String,
+	email: String,
+	created_date: Date,
+	updated_date: Date,
+	active_hash: String,
+	NbMandats : Number,
+	password: { type: String, required: true }
+});
+
+
+//methods ======================
+//generating a hash
+userSchema.methods.generateHash = function(password) {
+ return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+//checking if password is valid
+userSchema.methods.validPassword = function(password) {
+ return bcrypt.compareSync(password, this.password);
+};
+
+//create the model for users and expose it to our app
+module.exports = mongoose.model('ex_users', userSchema);
