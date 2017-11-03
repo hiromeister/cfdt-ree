04687fd826a8442b3351b7dfb6512daad75cf189
Makayoshin974 <houssenabasbay@gmail.com>
2017-11-03 10:27:26 +0400

    redirect login ok

--- config/permissions.js
+++ config/permissions.js
@@ -7,9 +7,12 @@ var permissions = new ConnectRoles({
         if (~ accept.indexOf('html')) {
             //If the user doesn't have permissions but has the "user" role 
             //then he is redirected to the user home page
+            if (req.user === undefined) {res.redirect('/login');}
             if(req.user.role === 'user'){
             	res.redirect('/homeVoter'); // TODO : change the url for the user homepage
-            }else{
+
+            }
+            else{
             	//If there is no user logged in then go to the login page
             	res.redirect('/login');
             }
